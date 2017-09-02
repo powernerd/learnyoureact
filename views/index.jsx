783ba821c8d4c39e5803591677113fede3faa11e
Andy <rsaman@powernerdsoftware.com>
2017-09-02 14:56:31 -0700

    exercises 1 and 2

---
+++ views/index.jsx
@@ -0,0 +1,34 @@
+
+    import React from 'react';
+
+    export default class TodoBox extends React.Component {
+        render() {
+            return (
+                <div className="todoBox">
+                    <h1>Todos</h1>
+                    <TodoList />
+                    <TodoForm />
+                </div>
+            );
+        }
+    }
+
+    class TodoList extends React.Component {
+      render () {
+        return (
+          <div className="todoList">
+            I am a TodoList.
+          </div>
+        )
+      }
+    }
+
+    class TodoForm extends React.Component {
+      render () {
+        return (
+          <div className="todoForm">
+            I am a TodoForm.
+          </div>
+        )
+      }
+    }
